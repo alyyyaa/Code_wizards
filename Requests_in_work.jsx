5a95952724b747a5fc5aac9479230555c31d7585
alyyyaa <116183100+alyyyaa@users.noreply.github.com>
2024-06-26 17:19:30 +0500

    Update Requests_in_work.jsx

--- Requests_in_work.jsx
+++ Requests_in_work.jsx
@@ -1,93 +1,123 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import DataTable from 'react-data-table-component';
-import css from "../styles/Requests_in_work.css";
-import { BrowserRouter as Router, Link } from 'react-router-dom'; 
 import { Button } from 'react-bootstrap';
+import { CiViewList } from "react-icons/ci";
+import { IoIosSearch } from "react-icons/io";
+import '../styles/Personal_account.css';
+import '../styles/DataTable.css';
+import Menu from "../pages/Menu.jsx"
+import Pagination from "../pages/Pagination.jsx"; 
 
-function Requests_in_work() {
-    const [searchValue, setSearchValue] = useState(""); // Состояние для отслеживания ввода пользователя
+function PersonalAccount() {
+    const data = [
+        { id: 1, date: '21.03.2024', name: 'Аноним', topic: 'Уборка', category: 'Уборка', action: 'Просмотр' },
+        { id: 2, date: '22.03.2024', name: 'Анатолий Некрасов', action: 'Просмотр' },
+        { id: 3, date: '23.03.2024', name: 'Иван Иванов', action: 'Просмотр' },
+        { id: 4, date: '24.03.2024', name: 'Петр Петров', action: 'Просмотр' },
+        { id: 5, date: '25.03.2024', name: 'Сергей Сергеев', action: 'Просмотр' },
+        { id: 6, date: '26.03.2024', name: 'Алексей Алексеев', action: 'Просмотр' },
+        { id: 7, date: '27.03.2024', name: 'Мария Маринова', action: 'Просмотр' },
+        { id: 8, date: '28.03.2024', name: 'Ольга Ольгина', action: 'Просмотр' },
+        { id: 9, date: '29.03.2024', name: 'Николай Николаев', action: 'Просмотр' },
+        { id: 10, date: '30.03.2024', name: 'Александр Александров', action: 'Просмотр' },
+        { id: 11, date: '30.03.2024', name: 'Александр Александров', action: 'Просмотр' },
+    ];
 
-    // Обработчик изменения ввода пользователя
+    const [searchValue, setSearchValue] = useState("");
+    const [records, setRecords] = useState(data);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [recordsPerPage] = useState(5);
+    const [iconActive, setIconActive] = useState(false);
+
     function handleChange(event) {
         setSearchValue(event.target.value);
     }
 
-    const placeholder = searchValue.length === 0 ? "Найти обращение...": "";
+    const placeholder = searchValue.length === 0 ? "Найти обращение..." : "";
 
     const columns = [
-        {
-            name: <span className="table-header">Дата обращения</span>,
-            selector: 'date',
-            cell: row => <span className="date-cell">{row.date}</span>
-        },
-        {
-            name: <span className="table-header">Тема</span>,
-            selector: 'topic',
-            cell: row => <span className="topic-cell">{row.topic}</span>
-        },
-        {
-            name: <span className="table-header">Отправитель</span>,
-            selector: 'name',
-            cell: row => <span className="name-cell">{row.name}</span>
-        },
-        {
-            name: <span className="table-header">Действие</span>,
-            cell: row => <Button variant="primary" className="custom-button">Просмотр</Button>
-        }
+        { name: <span className="custom-table-header">Дата обращения</span>, selector: 'date', cell: row => <span className="custom-date-cell">{row.date}</span> },
+        { name: <span className="custom-table-header">Отправитель</span>, selector: 'name', cell: row => <span className="custom-name-cell">{row.name}</span> },
+        { name: <span className="custom-table-header">Тема</span>, selector: 'topic', cell: row => <span className="custom-name-cell">{row.topic}</span> },
+        { name: <span className="custom-table-header">Категория</span>, selector: 'category', cell: row => <span className="custom-name-cell">{row.category}</span> },
+        { name: <span className="custom-table-header">Действие</span>, cell: row => <Button variant="primary" className="custom-action-button">Просмотр</Button> }
     ];
-    const data = [
-        {
-            id: 1,
-            date: '21.03.2024',
-            topic: 'Сломан принтер',
-            name: 'Аноним',
-            action: 'Просмотр'
-        },
-        {
-            id: 2,
-            date: '22.03.2024',
-            topic: 'Не работает Wi-Fi',
-            name: 'Анатолий Некрасов',
-            action: 'Просмотр'
-        }
 
-    ]
-    const [records, setRecords] = useState(data);
-
-    function handleFilter(event){
+    function handleFilter(event) {
         const value = event.target.value.toLowerCase();
         const newData = data.filter(row =>
             row.date.toLowerCase().includes(value) ||
-            row.topic.toLowerCase().includes(value) ||
-            row.name.toLowerCase().includes(value)
-
+            row.name.toLowerCase().includes(value) ||
+            (row.topic && row.topic.toLowerCase().includes(value)) ||
+            (row.category && row.category.toLowerCase().includes(value))
         );
         setRecords(newData);
+        setCurrentPage(1); // Сброс текущей страницы на первую после фильтрации
     }
+    
+
+    const handleIconClick = () => {
+        setIconActive(!iconActive);
+    };
+
+    const handlePageChange = (pageNumber) => {
+        setCurrentPage(pageNumber);
+    };
+
+    const indexOfLastRecord = currentPage * recordsPerPage; // индекс последней записи
+    const indexOfFirstRecord = indexOfLastRecord - recordsPerPage; // индекс первой записи
+    const currentRecords = records.slice(indexOfFirstRecord, indexOfLastRecord);
+
+    const totalPages = Math.ceil(records.length / recordsPerPage); // общее количество страниц
+
     return (
-        <div className='container mt-5'>
-            <Router>
-                <div className="header">
-                    <div className="left">
-                    <Link to="/" className="back-button">Назад</Link>
+        <div className="personal-account-container">
+            <Menu />
+            <div className="content-p">
+                <div className='container mt-5'>
+                    <div className="header">
+                        <div className="left"></div>
+                        <div className="right">
+                            <input type="text" onChange={handleFilter} className="custom-search-input" placeholder={placeholder} />
+                            <button className="custom-search-button" onClick={handleFilter}>
+                                Поиск
+                            </button>
+                        </div>
                     </div>
-                    <div className="right">
-                        <input type="text" onChange={handleFilter} className="search-input" placeholder={placeholder} />
-                        <button className="search-button">Поиск</button>
+                    <div className="data-section">
+                        <div className="custom-table-name">
+                            Новые обращения
+                            <button
+                                className={`custom-icon-button ${iconActive ? 'active' : ''}`}
+                                onClick={handleIconClick}
+                                title="Показать все обращения"
+                            >
+                                <CiViewList size={30} className="icon" />
+                            </button>
+                            <button
+                                className={`custom-search-icon ${iconActive ? 'active' : ''}`}
+                                onClick={handleIconClick}
+                                title="Найти одинаковые обращения"
+                            >
+                                <IoIosSearch size={30} className="icon" />
+                            </button>
+                        </div>
+                        <DataTable
+                            columns={columns}
+                            data={currentRecords}
+                            fixedHeader
+                            className="custom-data-table"
+                        />
+                        <Pagination
+                            currentPage={currentPage}
+                            totalPages={totalPages}
+                            handlePageChange={handlePageChange}
+                        />
                     </div>
                 </div>
-            </Router>
-            <div className="data-section">
-                <h1 className="table-name">Обращения в работе</h1>
-                <DataTable
-                    columns={columns}
-                    data={records}
-                    fixedHeader
-                    className="data-table"
-                ></DataTable>
-                </div>
+            </div>
         </div>
     );
 }
 
-export default Requests_in_work;
+export default PersonalAccount;
