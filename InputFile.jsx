89a20496366b21505692fb2c137d22bffd74ed33
alyyyaa <116183100+alyyyaa@users.noreply.github.com>
2024-06-27 13:41:12 +0500

    Update InputFile.jsx

--- InputFile.jsx
+++ InputFile.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import { MdFileDownload } from "react-icons/md";
-import '../styles/styles.css';
+import '../comps/styles.css';
 
 export const InputFile = ({ accept, multiple, files, setFiles }) => {
     const handleChange = (event) => {
-        const selectedFiles = event.target.files;
-        setFiles([...selectedFiles]);
+        const selectedFiles = Array.from(event.target.files);
+        if (files.length + selectedFiles.length > 3) {
+            alert("Вы не можете загрузить больше 3 фотографий");
+            return;
+        }
+        setFiles(prevFiles => [...prevFiles, ...selectedFiles]);
+    };
+
+
+    const handleDelete = (index) => {
+        const updatedFiles = [...files];
+        updatedFiles.splice(index, 1);
+        setFiles(updatedFiles);
     };
 
     return (
@@ -14,6 +25,7 @@ export const InputFile = ({ accept, multiple, files, setFiles }) => {
             {files && files.length > 0 && files.map((file, i) => (
                 <p className="input-file-info" key={i}>
                     {file.name}
+                    <span className="delete-icon" onClick={() => handleDelete(i)}>✕</span>
                 </p>
             ))}
         </div>
